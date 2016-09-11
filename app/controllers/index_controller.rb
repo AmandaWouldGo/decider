4bbd3da089b92a4197139e0099a7f8b6896d8249
codejlo <jsl728@Jasons-MacBook-Air.local>
2016-09-11 04:40:05 -0700

    Integrate twilio and Watson; Google results need updating

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,42 +1,83 @@
 require 'twilio-ruby'
 require 'awesome_print'
 
-# get '/inbound' do
-
-#   content_type 'text/xml'
-#   "<Response><Message>Got your message</Message></Response>"
-# end
-
 post '/inbound' do
   incoming = {message: params["Body"], sender: params["From"]}
 
-  ap incoming
+  user = User.find_by(phone_number: incoming[:sender])
 
-  content_type 'text/xml'
+  if user
+    time_since_contact = Time.new - user.updated_at
+    ap time_since_contact
 
-  response = Twilio::TwiML::Response.new do |r|
-    r.Message 'Hey thanks for messageing me!'
+    if time_since_contact > 1800
+      convo = WatsonConversations.new()
+      ap response = convo.start_convo(incoming[:message])
+
+      user.update(last_context: response["context"])
+      
+      message_back = Twilio::TwiML::Response.new do |r|
+        r.Message "#{response["output"]["text"][0]}"
+      end
+
+      message_back.to_xml
+    else
+      last_context = user.last_context
+      convo = WatsonConversations.new()
+      ap response = convo.continue_convo(incoming[:message], last_context)
+
+      user.update(last_context: response["context"])
+      response_text = response["output"]["text"][0]
+      if response_text[0..16] == "Finding food near"
+        ap "Googling ========================"
+        ap incoming[:message]
+        location_requested = incoming[:message]
+          locations = {
+            "soma" => "37.782623,-122.397372",
+            "mission" => "37.782623,-122.397372",
+            "north beach" => "37.799485,-122.408739"
+          }
+        search_params = {
+          location: locations[location_requested],
+          radius: "500",
+          types: "restaurant",
+          opennow: true,
+          key: ENV['GOOGLE_PLACES_API_KEY']
+        }
+
+        ap search = GooglePlaces.new(search_params)
+        ap search_to_parse = search.nearby
+        ap results = parse_nearby(search_to_parse)
+        ap result = sample_place(results)
+
+      else
+
+      end
+
+      message_back = Twilio::TwiML::Response.new do |r|
+        r.Message "#{response_text}"
+      end
+
+      message_back.to_xml
+    end
+  else
+    user = User.create(phone_number: incoming[:sender])
+    convo = WatsonConversations.new()
+    ap response = convo.start_convo(incoming[:message])
+
+    user.update(last_context: response["context"])
+      
+    message_back = Twilio::TwiML::Response.new do |r|
+      r.Message "#{response["output"]["text"][0]}"
+    end
+
+    message_back.to_xml
   end
-
-  response.to_xml
 end
 
 
-  # to = params["to"]
-  # message = params["body"]
 
-  # client = Twilio::REST::Client.new(
-  # ENV["TWILIO_ACCOUNT_SID"],
-  # ENV["TWILIO_AUTH_TOKEN"]
-  # )
-
-  # client.messages.create(
-  # to: "+14156999097",
-  # from: "+14155781513",
-  # body: "Yo yo" #message
-  # )
-
-
+### EXAMPLE CODE ###
 
 # post '/receive_sms' do
 #   content_type 'text/xml'
